8bda05410d3166aef12f718b586c1f709ad9c189
Kleber Bonassi <kbonassi@gmail.com>
2022-04-12 20:39:07 -0300

    Projeto criado

---
+++ src/main/java/com/acme/springbootn64/SpringbootN64Application.java
@@ -0,0 +1,13 @@
+package com.acme.springbootn64;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootN64Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootN64Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/acme/springbootn64/SpringbootN64ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acme.springbootn64;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootN64ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
